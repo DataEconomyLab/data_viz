820829b0a4f579208e9016ae25930e7ef78cc733
DataEconomyLab <dataeconomylab@gmail.com>
2021-05-12 13:39:54 +0900

    Update data_viz_report.r

--- pgm/data_viz_report.r
+++ pgm/data_viz_report.r
@@ -12,6 +12,8 @@ abline(lm(anscombe$y3~anscombe$x3), col="red")
 plot(anscombe$x4, anscombe$y4, pch=20, xlim=c(0, 20), ylim=c(0, 20), col="blue", xlab="x4", ylab="y4", main="anscombe4")
 abline(lm(anscombe$y4~anscombe$x4), col="red")
 
+#############################################################################################################################
+
 knitr::opts_chunk$set(echo = TRUE)
 knitr::opts_chunk$set(fig.align = "center")
 knitr::opts_chunk$set(warning = FALSE)
@@ -57,3 +59,19 @@ c4 <- ggplot(data=covid19FR, aes(x=as.Date(dateRep, format="%d/%m/%Y"), y=cases)
 	theme_bw(base_family = "NanumGothic")
 
 grid.arrange(c1, c2, c3, c4, ncol=2, nrow=2)
+
+#############################################################################################################################
+
+par(mfrow=c(2,2))
+
+plot(anscombe$x1, anscombe$y1, pch=20, xlim=c(0, 20), ylim=c(0, 20), col="blue", xlab="x1", ylab="y1", main="anscombe1")
+abline(lm(anscombe$y1~anscombe$x1), col="red")
+
+plot(anscombe$x2, anscombe$y2, pch=20, xlim=c(0, 20), ylim=c(0, 20), col="blue", xlab="x2", ylab="y2", main="anscombe2")
+abline(lm(anscombe$y2~anscombe$x2), col="red")
+
+plot(anscombe$x3, anscombe$y3, pch=20, xlim=c(0, 20), ylim=c(0, 20), col="blue", xlab="x3", ylab="y3", main="anscombe3")
+abline(lm(anscombe$y3~anscombe$x3), col="red")
+
+plot(anscombe$x4, anscombe$y4, pch=20, xlim=c(0, 20), ylim=c(0, 20), col="blue", xlab="x4", ylab="y4", main="anscombe4")
+abline(lm(anscombe$y4~anscombe$x4), col="red")
